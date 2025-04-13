26b1aa473ba4f043c516f4523f060e4729dc4a21
Nguyễn Công Thịnh <nguyencongthinh17122006@gmail.com>
2025-04-13 10:23:25 +0800

    ChieuCaoCay.c

--- ChieuCaoCay.c
+++ ChieuCaoCay.c
@@ -5,7 +5,7 @@
 int ChieuCaoCay(TREE t)
 {
     if (t == NULL)
-        return 0;
+        return -1;
     int leftHeight = ChieuCaoCay(t->pLeft);
     int rightHeight = ChieuCaoCay(t->pRight);
     return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
