1895697d9440545c820147fe2875bd94e47555ba
Nguyễn Công Thịnh <nguyencongthinh17122006@gmail.com>
2025-04-13 11:30:58 +0800

    Update DiTimNodeTheMang.c

--- DiTimNodeTheMang.c
+++ DiTimNodeTheMang.c
@@ -2,10 +2,14 @@
 #include "BST.h"
 
 
-void DiTimNodeTheMang(NODE** x, NODE** y) {
-    if ((*y)->pLeft != NULL) {
+void DiTimNodeTheMang(NODE** x, NODE** y) 
+{
+    if ((*y)->pLeft != NULL) 
+    {
         DiTimNodeTheMang(x, &(*y)->pLeft);
-    } else {
+    } 
+    else 
+    {
         (*x)->data = (*y)->data;
         NODE* temp = *y;
         *y = (*y)->pRight;
